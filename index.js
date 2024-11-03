021166fcb495be066e45356af3079baa21ca8ce9
gondsachin40 <162681936+gondsachin40@users.noreply.github.com>
2024-11-03 17:37:03 +0530

    stage 2 creation winner cordinates

--- index.js
+++ index.js
@@ -1,16 +1,20 @@
 var canvas = document.querySelector("canvas");
-let enemy  = [];
+let enemy = [];
 let platforms = [];
+let obstacles = [];
+let winners = [];
+let current_stage = 'stage1';
+let winncnt = 0;
 // canvas.height = 1300;
 canvas.height = 1300;
 // canvas.height = document.body.clientHeight;
 // canvas.width = document.body.clientWidth;
 canvas.width = 2200;
-let show_hidden = true;
+let show_hidden = false;
 var c = canvas.getContext("2d");
 const gravity = 0.5;
 let notover = false;
-let scroll_speed = 50;
+let scroll_speed = 15;
 let winn = false;
 var gokupos = new Image();
 gokupos.src = "gokupos.png";
@@ -67,27 +71,24 @@ class Enemy {
   }
   draw() {
     c.fillStyle = "black";
-    if(this.direction)
-    c.drawImage(saiba ,this.t, 0 , 100 , 100 , this.position.x , this.position.y + 10, this.width, this.height);
+    if (this.direction)
+      c.drawImage(saiba, this.t, 0, 100, 100, this.position.x, this.position.y + 10, this.width, this.height);
     else
-    c.drawImage(saiba ,this.t, 100 , 100 , 100 , this.position.x , this.position.y + 10, this.height, this.height);
-    if(frame % 5 === 0)
-    {
+      c.drawImage(saiba, this.t, 100, 100, 100, this.position.x, this.position.y + 10, this.height, this.height);
+    if (frame % 5 === 0) {
       this.t += 100;
     }
     enemyframe++;
     this.t %= 700;
   }
   update() {
-    if(this.visible)
-    {
-    this.draw();
-    if (this.position.x > this.pos.end || this.position.x < this.pos.start)
-    {
-      this.velocity.x = -this.velocity.x;
-      this.direction = !this.direction;
-    }
-    this.position.x += this.velocity.x;
+    if (this.visible) {
+      this.draw();
+      if (this.position.x > this.pos.end || this.position.x < this.pos.start) {
+        this.velocity.x = -this.velocity.x;
+        this.direction = !this.direction;
+      }
+      this.position.x += this.velocity.x;
     }
   }
 }
@@ -125,38 +126,34 @@ class Goku {
     this.t += 100;
     this.t = this.t % 800;
   }
-  kamehameha(){
+  kamehameha() {
     fire_x = this.position.x + this.width;
-    fire_y = this.position.y + 40;   
-    if(kamehameha_time < 150)
-    c.drawImage( kamehameha,this.k,0,this.kame, 100,this.position.x,this.position.y,k_width,this.height);
-    else
-    {
-    goku.standing();  
+    fire_y = this.position.y + 40;
+    if (kamehameha_time < 150)
+      c.drawImage(kamehameha, this.k, 0, this.kame, 100, this.position.x, this.position.y, k_width, this.height);
+    else {
+      goku.standing();
     }
     kamehameha_time++;
     if (frame % 10 == 0) {
-    if (this.k != 900)
-      { 
-      this.k += 100;
-      k_width = 150;
-      this.kame = 100; 
-      time_of_kamehama = 0; 
-    }
-    else
-    {
-      if(time_of_kamehama < 10)
-      {
-       k_width = 600;
-       this.kame = 300;
-       time_of_kamehama++;
+      if (this.k != 900) {
+        this.k += 100;
+        k_width = 150;
+        this.kame = 100;
+        time_of_kamehama = 0;
       }
+      else {
+        if (time_of_kamehama < 10) {
+          k_width = 600;
+          this.kame = 300;
+          time_of_kamehama++;
+        }
+      }
+      headmoveframe += this.k;
     }
-    headmoveframe += this.k;
-  }
-  this.k = this.k % 1200;
-  frame += 1;
-  frame %= 10;
+    this.k = this.k % 1200;
+    frame += 1;
+    frame %= 10;
   }
   standing() {
     c.drawImage(
@@ -268,7 +265,7 @@ class Platform {
   }
 }
 class Obj {
-  constructor({ x, y, image , width , height}) {
+  constructor({ x, y, image, width, height }) {
     this.position = {
       x: x,
       y: y,
@@ -317,13 +314,7 @@ const jumpaudio = new Audio("Punnet.mp3");
 const goku_screaming = new Audio("goku_screaming.mp3");
 
 let obj
-const obstacles = [
-  new Obstacle({ x: -900, y: 900, w: 900, h: 20 }),
-  new Obstacle({ x: 1240, y: 820, w: 520, h: 20 }),
-  new Obstacle({ x: 6200, y: 1320, w: 8065, h: 20 }),
-  new Obstacle({ x: 14830, y: 1340, w: 6600, h: 20 }),
-];
-const winners = [new Platform({ x: 18600, y: 1200, w: 900, h: 20 })];
+
 const message = new Platform({ x: 16640, y: 450, w: 300, h: 20 });
 const keys = {
   right: {
@@ -363,43 +354,46 @@ function animate() {
     obj.draw();
   });
   enemy.forEach((enemy) => {
-    if(enemy.position.x  + 600 < goku.position.x )
-    {
+    if (enemy.position.x + 600 < goku.position.x) {
       enemy.visible = true;
     }
     enemy.update();
   });
-  if(is_kamehameha_available)
-  {
+  if (is_kamehameha_available) {
     enemy.forEach((enemy) => {
-      if(enemy.position.x < fire_x + fire_w && fire_y  + fire_h >= enemy.position.y  && Math.abs(enemy.position.y - fire_y) < 150 && kamehameha_time > 100)
-      {
+      if (enemy.position.x < fire_x + fire_w && fire_y + fire_h >= enemy.position.y && Math.abs(enemy.position.y - fire_y) < 150 && kamehameha_time > 100) {
         enemy.visible = false;
       }
     });
   }
   goku.update();
-  c.drawImage(healthbar , 100 , 0 , 600 , 100);
+  c.drawImage(healthbar, 100, 0, 600, 100);
   c.fillStyle = "#22aef1";
-  c.fillRect(healthbar_pos_x, 38 , healthbar_width ,27);
-  if(show_hidden)
-  {
-  platforms.forEach((platform) => {
+  c.fillRect(healthbar_pos_x, 38, healthbar_width, 27);
+  if (show_hidden) {
+    platforms.forEach((platform) => {
       platform.update();
-  });
-  obstacles.forEach((obstacle) => {
+    });
+    obstacles.forEach((obstacle) => {
       obstacle.update();
-  });
-  winners.forEach((win) => {
+    });
+    winners.forEach((win) => {
       win.update();
-  });
-  message.update();
-}
+    });
+    message.update();
+  }
   if (notover) {
-    c.drawImage(losing , 0, 0 , canvas.width,canvas.height);
+    c.drawImage(losing, 0, 0, canvas.width, canvas.height);
   }
   if (winn) {
-    c.drawImage(winning , 0 , 0 , canvas.width , canvas.height);
+    current_stage = 'stage2';
+    winn = false;
+    winncnt++;
+    load()
+    // c.drawImage(winning, 0, 0, canvas.width, canvas.height);
+  }
+  if (winncnt === 2) {
+    c.drawImage(winning, 0, 0, canvas.width, canvas.height);
   }
   if (keys.right.pressed && goku.position.x < 400) {
     goku.velocity.x = 5;
@@ -491,7 +485,7 @@ function animate() {
     if (
       goku.position.x >= platform.position.x + platform.width &&
       goku.position.x + goku.velocity.x <=
-        platform.position.x + platform.width &&
+      platform.position.x + platform.width &&
       goku.position.y + goku.height > platform.position.y &&
       goku.position.y < platform.position.y + platform.height
     ) {
@@ -499,70 +493,63 @@ function animate() {
     }
   });
   enemy.forEach((enemy) => {
-    if(enemy.visible)
-    {
-    if (
-      goku.position.x <= enemy.position.x  + enemy.velocity.x &&
-      goku.position.x + goku.width + goku.velocity.x >= enemy.position.x  + enemy.velocity.x + 100 &&
-      goku.position.y + goku.height > enemy.position.y &&
-      goku.position.y < enemy.position.y + enemy.height 
-    ) {
-      if(healthbar_time === 0 || (healthbar_time > 10 && healthbar_time < 12)|| healthbar_time > 20)
-        {
-        if(healthbar_state === 1)
-        {
-        healthbar_pos_x = 490;
-        healthbar_width = 120;
-        healthbar_state++;
+    if (enemy.visible) {
+      if (
+        goku.position.x <= enemy.position.x + enemy.velocity.x &&
+        goku.position.x + goku.width + goku.velocity.x >= enemy.position.x + enemy.velocity.x + 100 &&
+        goku.position.y + goku.height > enemy.position.y &&
+        goku.position.y < enemy.position.y + enemy.height
+      ) {
+        if (healthbar_time === 0 || (healthbar_time > 10 && healthbar_time < 12) || healthbar_time > 20) {
+          if (healthbar_state === 1) {
+            healthbar_pos_x = 490;
+            healthbar_width = 120;
+            healthbar_state++;
+          }
+          else if (healthbar_state === 2) {
+            healthbar_pos_x = 300;
+            healthbar_width = 310;
+            healthbar_state++;
+          }
+          else if (healthbar_state === 3) {
+            healthbar_pos_x = 220;
+            healthbar_width = 390;
+            notover = true;
+          }
         }
-        else if(healthbar_state === 2)
-        {
-          healthbar_pos_x = 300;
-          healthbar_width = 310;
-          healthbar_state++;
-        }
-        else if(healthbar_state === 3){
-          healthbar_pos_x = 220;
-          healthbar_width = 390;
-          notover = true;
+        healthbar_time++;
+      }
+      if (
+        goku.position.x >= enemy.position.x + enemy.velocity.x &&
+        goku.position.x + goku.velocity.x <= enemy.position.x + enemy.velocity.x + 50 &&
+        goku.position.y + goku.height > enemy.position.y &&
+        goku.position.y < enemy.position.y + enemy.height
+      ) {
+        if (healthbar_time === 0 || (healthbar_time > 10 && healthbar_time < 12) || healthbar_time > 20) {
+          if (healthbar_state === 1) {
+            healthbar_pos_x = 490;
+            healthbar_width = 120;
+            healthbar_state++;
+          }
+          else if (healthbar_state === 2) {
+            healthbar_pos_x = 300;
+            healthbar_width = 310;
+            healthbar_state++;
+          }
+          else if (healthbar_state === 3) {
+            healthbar_pos_x = 220;
+            healthbar_width = 390;
+            notover = true;
+          }
         }
       }
-      healthbar_time++;
     }
-    if (
-      goku.position.x >= enemy.position.x  + enemy.velocity.x &&
-      goku.position.x + goku.velocity.x <= enemy.position.x  + enemy.velocity.x + 50 &&
-      goku.position.y + goku.height > enemy.position.y &&
-      goku.position.y < enemy.position.y + enemy.height 
-    ) {
-      if(healthbar_time === 0 || (healthbar_time > 10 && healthbar_time < 12)|| healthbar_time > 20)
-        {
-        if(healthbar_state === 1)
-        {
-        healthbar_pos_x = 490;
-        healthbar_width = 120;
-        healthbar_state++;
-        }
-        else if(healthbar_state === 2)
-        {
-          healthbar_pos_x = 300;
-          healthbar_width = 310;
-          healthbar_state++;
-        }
-        else if(healthbar_state === 3){
-          healthbar_pos_x = 220;
-          healthbar_width = 390;
-          notover = true;
-        }
-    }
-  }
-  }
-});
+  });
   obstacles.forEach((obstacle) => {
     if (
       goku.position.y + goku.height <= obstacle.position.y &&
       goku.position.y + goku.height + goku.velocity.y >=
-        obstacle.position.y - 10 &&
+      obstacle.position.y - 10 &&
       goku.position.x + goku.width >= obstacle.position.x &&
       goku.position.x <= obstacle.position.x + obstacle.width
     ) {
@@ -584,7 +571,7 @@ function animate() {
   if (
     goku.position.y + goku.height <= message.position.y &&
     goku.position.y + goku.height + goku.velocity.y >=
-      message.position.y - 10 &&
+    message.position.y - 10 &&
     goku.position.x + goku.width >= message.position.x &&
     goku.position.x <= message.position.x + message.width
   ) {
@@ -597,21 +584,22 @@ function animate() {
   }
   c.stroke();
 }
-
 async function load() {
-  
-  let stage = 'stage2';
+
+  let stage = current_stage;
   const enemyURL = `${window.location.href}${stage}/enemy.json`;
   const platformURL = `${window.location.href}${stage}/platform.json`;
+  const obstacleURL = `${window.location.href}${stage}/obstacle.json`;
+  const winnerURL = `${window.location.href}${stage}/winner.json`;
   console.log(``)
 
   let image = new Image();
-image.src = `${stage}/temp.png`;
-   if(stage === 'stage1')
-   obj = [new Obj({ x: 0, y: 0, image  , width : 22800 , height : canvas.height})];
-   else if(stage === 'stage2')
-    obj = [new Obj({ x: 0, y: 0, image  , width : 22800 , height : 1500 })];
-    await fetch(enemyURL).then(val => {
+  image.src = `${stage}/temp.png`;
+  if (stage === 'stage1')
+    obj = [new Obj({ x: 0, y: 0, image, width: 22800, height: canvas.height })];
+  else if (stage === 'stage2')
+    obj = [new Obj({ x: 0, y: 0, image, width: 22800, height: 1500 })];
+  await fetch(enemyURL).then(val => {
     return val.json()
   }).then(val => {
     enemy = [];
@@ -625,11 +613,26 @@ image.src = `${stage}/temp.png`;
   }).then(val => {
     platforms = [];
     val.forEach(x => {
-      platforms.push(new Platform({ x: x.x, y: x.y,   w: x.w, h: x.h }));
+      platforms.push(new Platform({ x: x.x, y: x.y, w: x.w, h: x.h }));
+    })
+  })
+  await fetch(winnerURL).then(val => {
+    return val.json()
+  }).then(val => {
+    winners = [];
+    val.forEach(x => {
+      winners.push(new Platform({ x: x.x, y: x.y, w: x.w, h: x.h }));
+    })
+  })
+  await fetch(obstacleURL).then(val => {
+    return val.json()
+  }).then(val => {
+    obstacles = [];
+    val.forEach(x => {
+      obstacles.push(new Obstacle({ x: x.x, y: x.y, w: x.w, h: x.h }));
     })
   })
 }
-
 load().then(() => {
   animate();
 });
@@ -637,9 +640,9 @@ document.addEventListener("keydown", (event) => {
   let keycode = event.keyCode;
   switch (keycode) {
     case 32: //space
-     action = "kamehameha";
-     is_kamehameha_available = true;
-     break;
+      action = "kamehameha";
+      is_kamehameha_available = true;
+      break;
     case 37:
       keys.left.pressed = true;
       action = "moveleft";
@@ -676,13 +679,13 @@ document.addEventListener("keyup", (event) => {
   let keycode = event.keyCode;
   switch (keycode) {
     case 32: //space
-     action = "standing";
-     is_kamehameha_available = false;
-     kamehameha_time = 0;
-     goku.kame = 100;
-     k_width = 150;
-     goku.k = 0;
-     break;
+      action = "standing";
+      is_kamehameha_available = false;
+      kamehameha_time = 0;
+      goku.kame = 100;
+      k_width = 150;
+      goku.k = 0;
+      break;
     case 37: //left
       keys.left.pressed = false;
       action = "standing";
